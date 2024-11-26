4c07920ca8616d82c1a278f71cac20a9f1db2cf2
SirajusSalekin12 <salekin.sirajus@northsouth.edu>
2024-11-26 09:52:54 +0600

    Update run_cnn.py

--- run_cnn.py
+++ run_cnn.py
@@ -8,25 +8,25 @@ def main():
     print("Running CNN...")
     start_time = time.time()
 
-    # Dataset setup
+    
     dataset = 'MNIST'
     datadir = './data'
     num_classes = 10
     input_shape = (28, 28, 1)
 
-    # Load dataset
+    
     (x_train, y_train), (x_test, y_test) = load_data(dataset, num_classes, datadir)
 
-    # Normalize and reshape data
+    
     x_train, x_test = x_train / 255.0, x_test / 255.0
     x_train = x_train.reshape(-1, *input_shape)
     x_test = x_test.reshape(-1, *input_shape)
 
-    # One-hot encode labels
+    
     y_train = tf.keras.utils.to_categorical(y_train, num_classes)
     y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
-    # Define CNN model
+    
     model = models.Sequential([
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
         layers.MaxPooling2D((2, 2)),
@@ -37,15 +37,15 @@ def main():
         layers.Dense(num_classes, activation='softmax')
     ])
 
-    # Compile and train the model
+    
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(x_train, y_train, epochs=5, batch_size=32, validation_split=0.1)
 
-    # Evaluate model
+    
     test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=2)
     elapsed_time = time.time() - start_time
 
-    # Save results
+    
     results = {
         "method": "CNN",
         "accuracy": test_accuracy,
